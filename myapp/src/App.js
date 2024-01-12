0e4d72d40b548a691187cc2c3728f59c0ac7a6c6
snehal-maharaj <snehal.technotery@gmail.com>
2024-01-12 19:19:45 +0530

    Initial commit for the first react app 2024

---
+++ myapp/src/App.js
@@ -0,0 +1,12 @@
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <p>loremcscscscscscscscscscscscscscscscscscscscscscs</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ myapp/src/index.js
@@ -0,0 +1,11 @@
+import React, { startTransition } from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
